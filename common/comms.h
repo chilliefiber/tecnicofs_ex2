776cec13e5a38d9e2cb5950eb54bc1c6a807767d
Pedro Matias <pedro.lisboa.matias@tecnico.ulisboa.pt>
2022-02-04 22:21:31 +0000

    Corrigir warning apos format

--- common/comms.h
+++ common/comms.h
@@ -1,6 +1,8 @@
 #ifndef COMMS_H
 #define COMMS_H
 
+#include <stddef.h> // after make fmt it complained it didn't know size_t
+
 // Note that these functions are very rudimentary: when writing to pipes
 // you need to make sure the len is PIPE_BUF or smaller
 
